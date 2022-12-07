a65958f146b01ffe7f01962f2dbcfd3b5068c425
Evan Voeltner <evanvoeltner@gmail.com>
2022-12-07 14:48:09 -0600

    feat: TaskInstanceTables

--- frontend/src/components/TaskInstanceTable/TaskInstanceTable.jsx
+++ frontend/src/components/TaskInstanceTable/TaskInstanceTable.jsx
@@ -1,38 +1,126 @@
 import React, { useState } from "react";
 
 const TaskInstanceTable = (props) => {
-  function determineDisplay(taskInstanceDate) {
+  function determineTaskInstancesToDisplay(taskInstanceDate) {
     let currentDate = new Date();
-    console.log(taskInstanceDate)
-    let taskInstanceDateArray = taskInstanceDate.split("-")
-    console.log(taskInstanceDateArray)
-
+    console.log(taskInstanceDate);
+    let taskInstanceDateArray = taskInstanceDate.split("-");
+    console.log(taskInstanceDateArray);
 
     if (props.recurring_pattern === 1) {
-      console.log("Daily Task Comparison ", taskInstanceDate, " === ", currentDate)
+      console.log(
+        "Daily Task Comparison ",
+        taskInstanceDate,
+        " === ",
+        currentDate
+      );
       if (taskInstanceDate === currentDate.toJSON().slice(0, 10)) {
         return true;
       }
     } else if (props.recurring_pattern === 2) {
-      console.log("Full Year" ,currentDate.getFullYear().toString())
-      console.log("Full Year" ,taskInstanceDateArray[0])
-      console.log("Full Month" , (currentDate.getMonth()+ 1).toString())
-      console.log("Full Date" , (taskInstanceDateArray[2]).toString())
+      console.log("Full Year", currentDate.getFullYear().toString());
+      console.log("Full Year", taskInstanceDateArray[0]);
+      console.log("Full Month", (currentDate.getMonth() + 1).toString());
+      console.log("Full Date", taskInstanceDateArray[2].toString());
       if (
         taskInstanceDateArray[0] === currentDate.getFullYear().toString() &&
-        taskInstanceDateArray[1] === (currentDate.getMonth()+ 1).toString() &&
+        taskInstanceDateArray[1] === (currentDate.getMonth() + 1).toString() &&
         taskInstanceDateArray[2] >= currentDate.getDate() &&
-        (taskInstanceDateArray[2] - 7) <= currentDate.getDate()
+        taskInstanceDateArray[2] - 7 <= currentDate.getDate()
       ) {
         return true;
       }
     } else if (props.recurring_pattern === 3) {
       if (
         taskInstanceDateArray[0] === currentDate.getFullYear().toString() &&
-        taskInstanceDateArray[1] === (currentDate.getMonth()+ 1).toString()
+        taskInstanceDateArray[1] === (currentDate.getMonth() + 1).toString()
       ) {
         return true;
       }
+    }
+  }
+
+  function determinePrecentageToCompletion() {
+    let currentDate = new Date();
+
+    if (props.recurring_pattern === 1) {
+      let todaysTaskInstancesArray = props.taskInstances.filter(
+        (taskInstanceToFilter) => {
+          if (
+            taskInstanceToFilter.date_to_be_completed ===
+            currentDate.toJSON().slice(0, 10)
+          ) {
+            return true;
+          }
+        }
+      );
+
+      let totalTaskInstances = todaysTaskInstancesArray.length;
+      let totalCompleteTaskInstances = todaysTaskInstancesArray.filter(
+        (taskInstanceToFilter) => {
+          if (taskInstanceToFilter.is_completed) {
+            return true;
+          }
+        }
+      ).length;
+
+      return (totalCompleteTaskInstances / totalTaskInstances) * 100;
+    } else if (props.recurring_pattern === 2) {
+      let thisWeeksTaskInstancesArray = props.taskInstances.filter(
+        (taskInstanceToFilter) => {
+          let taskInstanceDateArray =
+            taskInstanceToFilter.date_to_be_completed.split("-");
+
+          if (
+            taskInstanceDateArray[0] === currentDate.getFullYear().toString() &&
+            taskInstanceDateArray[1] ===
+              (currentDate.getMonth() + 1).toString() &&
+            taskInstanceDateArray[2] >= currentDate.getDate() &&
+            taskInstanceDateArray[2] - 7 <= currentDate.getDate() &&
+            taskInstanceToFilter.recurring_pattern === props.recurring_pattern
+          ) {
+            return true;
+          }
+        }
+      );
+
+      let totalTaskInstances = thisWeeksTaskInstancesArray.length;
+      let totalCompleteTaskInstances = thisWeeksTaskInstancesArray.filter(
+        (taskInstanceToFilter) => {
+          if (taskInstanceToFilter.is_completed) {
+            return true;
+          }
+        }
+      ).length;
+
+      return (totalCompleteTaskInstances / totalTaskInstances) * 100;
+    } else if (props.recurring_pattern === 3) {
+      let thisMonthsTaskInstancesArray = props.taskInstances.filter(
+        (taskInstanceToFilter) => {
+          let taskInstanceDateArray =
+            taskInstanceToFilter.date_to_be_completed.split("-");
+
+          if (
+            taskInstanceDateArray[0] === currentDate.getFullYear().toString() &&
+            taskInstanceDateArray[1] ===
+              (currentDate.getMonth() + 1).toString() &&
+            taskInstanceToFilter.recurring_pattern === props.recurring_pattern
+          ) {
+            return true;
+          }
+        }
+      );
+
+      let totalTaskInstances = thisMonthsTaskInstancesArray.length;
+      let totalCompleteTaskInstances = thisMonthsTaskInstancesArray.filter(
+        (taskInstanceToFilter) => {
+          if (taskInstanceToFilter.is_completed) {
+            return true;
+          }
+        }
+      ).length;
+
+      return (totalCompleteTaskInstances / totalTaskInstances) * 100;
     }
   }
 
@@ -65,7 +153,11 @@ const TaskInstanceTable = (props) => {
                   props.recurring_pattern &&
                 taskInstanceToFilter.is_completed === props.isCompleted
               ) {
-                if (determineDisplay(taskInstanceToFilter.date_to_be_completed)) {
+                if (
+                  determineTaskInstancesToDisplay(
+                    taskInstanceToFilter.date_to_be_completed
+                  )
+                ) {
                   return true;
                 }
               } else if (
@@ -99,7 +191,7 @@ const TaskInstanceTable = (props) => {
             })}
         </tbody>
       </table>
-      <div></div>
+      <div>{determinePrecentageToCompletion()}%</div>
     </div>
   );
 };
